32ef62dd73145ca9b6a3653acac6361a3624b0d8
Riwanlm <riwanlm@gmail.com>
2026-01-21 20:59:24 +0100

    Mise en place de prettier pour formatage code plus rapide + ajout section accueil + ajout marker des différents events sur map Leaflet

--- src/Accueil.tsx
+++ src/Accueil.tsx
@@ -1,13 +1,17 @@
 import useSWR from "swr";
 import { Map } from "./components/Map";
+import { Link } from "react-router-dom";
+import { CircleArrowRight } from "lucide-react";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-type TEvent = {
+export type TEvent = {
   id: number;
   title: string;
   description: string;
   location: string;
+  latitude: string;
+  longitude: string;
   date: string;
   id_user: string;
   created_at: string;
@@ -15,22 +19,30 @@ type TEvent = {
 };
 
 export const Accueil = () => {
-  const { data, error, isLoading } = useSWR<TEvent[]>(
+  const { data, isLoading } = useSWR<TEvent[]>(
     `http://localhost:3000/events`,
     fetcher
   );
-  if (data) console.log(data, "COUCOU");
 
   return (
     <div className="container mx-auto">
-      <div className="h-1500 bg-gray-600">
-        <h1>Projet BeSkwad 👋</h1>
-        {isLoading ? <p>C'est en train de LOAD ...</p> : null}
-        {error ? <div>ERROR</div> : null}
-        {data &&
-          data.map((event: TEvent) => <div key={event.id}>{event.title}</div>)}
-      </div>
-      <Map/>
+      <section className="h-80 w-auto bg-[url(../src/assets/annie-spratt-ocz2K0KrDKE-unsplash.jpg)] bg-cover bg-no-repeat bg-center flex flex-col items-center justify-center gap-4">
+        <h1 className="bg-black/30 border-none border-white/30 outline-white/30 shadow-xl/55 rounded-xl text-white text-center text-4xl uppercase font-extrabold p-4 italic">Marre de faire du sport en mode solo ? <br />
+          Rejoins l'aventure Beskwad !</h1>
+        <Link to="/inscription" >
+          <button className="btn bg-[#fcb523] border border-white rounded-4xl btn-xs hover:bg-white/30 hover:text-[#fcb523] hover:border-[#fcb523] sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl"> <CircleArrowRight size={24} />Inscrit toi par ici </button>
+        </Link>
+      </section>
+      <section className="h-80 w-auto">
+        SECTION 2
+      </section>
+      <section className="h-80 w-auto">
+        SECTION 3
+      </section>
+      <section>
+        <h2 className="text-2xl">Retrouver les derniers évènements ajouter :</h2>
+        {isLoading ? <p>Chargement ...</p> : <Map events={data} />}
+      </section>
     </div>
   );
 };
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,21 +1,37 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useRef } from "react";
+import type { TEvent } from "../Accueil";
 
-export const Map = () => {
+export const Map = ({ events }: { events: TEvent[] | undefined }) => {
 
   const mapRef = useRef<L.Map | null>(null);
 
   useEffect(() => {
+
+    if (events) {
+      console.log(events);
+    }
+
     if (mapRef.current) {
       return;
     }
 
-    mapRef.current = L.map('map').setView([47.253813973618605, -1.5348673111028772], 13);
+    const map = L.map('map').setView([46.656458, 2.437566], 6);
+    mapRef.current = map;
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(mapRef.current);
+
+
+    events?.forEach((e) => {
+      console.log(e);
+      const marker = L.marker([Number(e.latitude), Number(e.longitude)]).addTo(map);
+      marker.bindPopup(`Titre de l'évènement ${e.title}`)
+    })
+
 
     return () => {
       if (mapRef.current) {
@@ -23,7 +39,7 @@ export const Map = () => {
         mapRef.current = null;
       }
     };
-  }, []);
+  }, [events]);
 
-  return <div className="h-[400px] sm:h-[500px] md:h-[600px] w-full max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl mx-auto m-4" id="map"></div>
+  return <div className="h-[400px] sm:h-[500px] md:h-[600px] w-full max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl mx-auto mt-5 mb-10 rounded-xl" id="map"></div>
 }
